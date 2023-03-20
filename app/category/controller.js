23c2dfdf22a099d4ce507982eb8f18d0eca659af
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2023-03-20 22:22:06 +0700

    update

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,9 +1,30 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("index", {
-        title: "Express JS",
-      });
+      const category = await Category.find();
+
+      res.render("admin/category/view_category", { category });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/category/create");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      let category = await Category({ name });
+      await category.save();
+
+      res.redirect("/category");
     } catch (err) {
       console.log(err);
     }
